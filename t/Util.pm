0eea92ac33a7c1f21ed76f15bf3a57088e48edf1
meru_akimbo <merukatoruayu0@gmail.com>
2016-06-21 11:44:52 +0900

    skip test when no mysql

--- t/Util.pm
+++ t/Util.pm
@@ -3,19 +3,22 @@ use strict;
 use warnings;
 use utf8;
 
+use Test::More;
 use Test::mysqld;
 
 BEGIN {
-    my $mysqld = Test::mysqld->new(
-        my_cnf => {
-            'skip-networking' => '', # no TCP socket
-        }
-    ) or die $Test::mysqld::errstr;
+    eval {
+        my $mysqld = Test::mysqld->new(
+            my_cnf => {
+                'skip-networking' => '', # no TCP socket
+            }
+        ) or die $Test::mysqld::errstr;
 
-    $TEST_GUARDS::MYSQLD = $mysqld;
-    $ENV{TEST_MYSQL}     = $mysqld->dsn;
+        $TEST_GUARDS::MYSQLD = $mysqld;
+        $ENV{TEST_MYSQL}     = $mysqld->dsn;
 
-    END { undef $TEST_GUARDS::MYDSLD }
+        END { undef $TEST_GUARDS::MYDSLD }
+    } or plan(skip_all => 'mysql-server is required to this test');
 }
 
 1;
